28fb5bde8db802422a64122f97ba5060305ed3d9
Alexei Pankov <alexpnkv@gmail.com>
2019-03-19 01:58:44 +0300

    Refactoring, one sample prediction

--- mondrianforest.py
+++ mondrianforest.py
@@ -1,16 +1,15 @@
 import numpy as np
-from scipy.stats import expon, uniform
+from scipy.stats import expon, truncexpon, uniform
+
 
 # Based on Mondrian Forests: Efficient Online Random Forests
 # https://arxiv.org/pdf/1406.2673.pdf
 
 
+GAMMA = 1
+
+
 def data_ranges(data):
-    """
-    Computes l^x_{...} and u^x_{...}.
-    :param data: input matrix
-    :return: minimums and maximums by feature
-    """
     return np.min(data, axis=0), np.max(data, axis=0)
 
 
@@ -20,237 +19,270 @@ class MondrianTree:
         self.budget = budget
         self.classes = None
         self.class_indices = None
-        self.regions = 0
         self.root = None
+        self.X = None
+        self.y = None
+        self.fitted = False
 
-    # Algorithm 1 + online option
-    def fit(self, X, y, simulate_online=False):
+    # Algorithm 1 + fully online option
+    def fit(self, X, y, online=False):
+        self.X = X
+        self.y = y
         self.classes = np.unique(y)
         self.class_indices = {cls: i for i, cls in enumerate(self.classes)}
-        if not simulate_online:
-            self.root = MondrianBlock(X, None, self.budget, self)
-            self.batch_initialize_posterior_counts(X, y)
+        if not online:
+            self.root = MondrianBlock(X, y, parent=None, budget=self.budget, tree=self)
             self.compute_predictive_posterior()
         else:
-            self.root = MondrianBlock(X[:2], None, self.budget, self)
-            self.batch_initialize_posterior_counts(X[:2], y[:2])
-            for i in range(2, len(X)):
+            self.root = MondrianBlock(X[:2], y[:2], parent=None, budget=self.budget, tree=self)
+            for i in range(2, len(y)):
                 self.extend(X[i], y[i])
 
-    # Algorithm 3
-    def extend(self, x, y):
-        x_leaf = self.root.extend(x, y)
-        x_leaf.update_posterior_counts(y)
+        self.fitted = True
 
-    # Algorithm 5
-    def batch_initialize_posterior_counts(self, X, y):
-        queue = list(self.leaf_nodes)
-        while queue:
-            node = queue.pop()
-            print('init posterior,', node)
-            if not node.posterior_initialized:
-                node_indices = np.all(X >= node.lower, axis=1) & np.all(X <= node.upper, axis=1)
-                labels_in_node = y[node_indices]
-                label_distribution = np.zeros_like(self.classes)
-
-                if node.is_leaf:
-                    for i, cls in enumerate(self.classes):
-                        label_distribution[i] = np.count_nonzero(labels_in_node == cls)
-                else:
-                    label_distribution += node.left.label_distribution if node.left else 0
-                    label_distribution += node.right.label_distribution if node.right else 0
-
-                node.label_distribution = label_distribution
-                node.tables = np.minimum(label_distribution, 1)
-                node.posterior_initialized = True
-                if not node.parent:
-                    break
-                else:
-                    queue = [node.parent] + queue
-
-    # Algotithm 7
+    # Algorithm 7
     def compute_predictive_posterior(self):
         queue = [self.root]
         while queue:
             node = queue.pop()
-            if node is self.root:
+            if node.parent is None:
                 parent_posterior = np.ones_like(self.classes) / len(self.classes)  # H
             else:
                 parent_posterior = node.parent.posterior_predictive
 
-            node_c = node.label_distribution
-            node_tab = node.tables
+            class_counts = node.class_counts
+            tables = node.tables
             discount = node.discount
 
-            node.posterior_predictive = (node_c - discount * node_tab
-                                         + discount * np.sum(node_tab) * parent_posterior) / np.sum(node_c)
+            node.posterior_predictive = (class_counts - discount * tables
+                                         + discount * np.sum(tables) * parent_posterior) / np.sum(class_counts)
 
             if node.left:
                 queue = [node.left] + queue
             if node.right:
                 queue = [node.right] + queue
 
-    def leaf(self, x):
-        node = self.root
-        while not node.is_leaf:
-            if any(x < node.lower) or any(x > node.upper):
-                return None
-            if x[node.delta] <= node.delta:
-                node = node.left
+    # Algorithm 8
+    def predict(self, x):
+        assert len(x.shape) == 1  # prediction for single x for now
+
+        x += 1e-12  # dirty hack in case x is included in the training
+
+        current = self.root
+        pnsy = 1.
+        s = np.zeros_like(self.classes, dtype=np.float64)
+        while True:
+            cost_difference = current.cost - current._parent_cost()
+            eta = (np.maximum(x - current.upper, 0) + np.maximum(current.lower - x, 0)).sum()
+            psjx = -np.expm1(-eta * cost_difference)
+            if psjx > 0:
+                expected_discount = (eta / (eta + GAMMA)) * (-np.expm1(-(eta + GAMMA) * cost_difference)) \
+                           / (-np.expm1(-eta * cost_difference))
+
+                class_counts = tables = np.minimum(current.class_counts, 1)
+
+                if current.parent is None:
+                    tilde_parent_posterior = np.ones_like(self.classes) / len(self.classes)
+                else:
+                    tilde_parent_posterior = current.parent.posterior_predictive
+
+                posterior = (class_counts / np.sum(class_counts) - expected_discount * tables
+                             + expected_discount * tables.sum() * tilde_parent_posterior)
+                s += pnsy * psjx * posterior
+            if current.is_leaf:
+                s += pnsy * (1 - psjx) * current.posterior_predictive
+                return s
             else:
-                node = node.right
-        return node
+                pnsy *= 1 - psjx
+                if x[current.delta] <= current.xi:
+                    current = current.left
+                else:
+                    current = current.right
+
+    def extend(self, X, y):
+        self.root.extend(X, y)
 
 
 class MondrianBlock:
-    # tree pointer is needed only to add block to leaves
-    def __init__(self, data, parent, budget, tree: MondrianTree = None, fit=True):
+    def __init__(self, X, y, budget, parent=None, tree: MondrianTree = None, fit=True):
         assert tree
         self.tree = tree
-        self.number = tree.regions
-        tree.regions += 1
-
         self.parent = parent
         self.left = None
         self.right = None
-        self.is_leaf = True
         self.budget = budget
-        self.cost = 0 if not parent else parent.cost
         self.discount = 0
-
-        self.label_distribution = np.zeros_like(tree.classes)
-        self.tables = np.zeros_like(tree.classes)  # "Chinese restaurants" notation from the paper
-        self.posterior_predictive = np.zeros_like(tree.classes)
-        self.posterior_initialized = False
-
-        self.delta = None
-        self.xi = None
+        self.lower = np.zeros(X.shape[1]) if X is not None else None
+        self.upper = np.zeros_like(self.lower) if X is not None else None
+        self.sides = np.zeros_like(self.lower) if X is not None else None
+        self.class_counts = np.zeros_like(self.tree.classes)  # not exactly _counts_
+        self.tables = np.zeros_like(self.tree.classes)  # see Chinese restaurants notation in the paper
+        self.is_leaf = True  # will be set to False when needed
 
         if fit:
-            self.lower, self.upper = data_ranges(data)
-            self.sides = self.upper - self.lower
-            self._fit(data)
+            self._fit(X, y)
 
-    def __repr__(self):
-        return f'MB {self.number}{" (l)" if self.is_leaf else ""}'
-
-    # Algorithm 2
-    def _fit(self, data):
-        # one point in block
-        if self.sides.sum() <= 0:
-            self.is_leaf = True
-            self.tree.leaf_nodes.add(self)
-            return
-
-        split_cost = expon.rvs(scale=(1 / self.sides.sum()))
-        parent_cost = self.parent.cost if self.parent else 0.
-        if parent_cost + split_cost < self.budget:
-            # choose split dimension delta and location xi
-            delta = np.random.choice(np.arange(data.shape[1]), p=(self.sides / self.sides.sum()))
-            xi = uniform.rvs(loc=self.lower[delta], scale=self.sides[delta])
-
-            # save split parameters
-            self.cost = parent_cost + split_cost
-            self.delta = delta
-            self.xi = xi
-
-            # perform an actual split
-            data_left = data[data[:, delta] <= xi]
-            data_right = data[data[:, delta] > xi]
-
-            # sample children
-            self.left = MondrianBlock(data_left, self, self.budget, self.tree)
-            self.right = MondrianBlock(data_right, self, self.budget, self.tree)
-            self.is_leaf = False
+    def _parent_cost(self):
+        if self.parent is None:
+            return 0.
         else:
-            self.cost = self.budget
-            self.is_leaf = True
-            self.tree.leaf_nodes.add(self)
+            return self.parent.cost
 
-        self.discount = np.exp(-(self.cost - parent_cost))
-
-    # Algorithm 4
-    def extend(self, x, y=None):
-        el = np.maximum(self.lower - x, np.zeros_like(x))
-        eu = np.maximum(x - self.upper, np.zeros_like(x))
-        sum_e = el + eu
-
-        split_cost = expon.rvs(scale=(1 / sum_e.sum()))
-        parent_cost = self.parent.cost if self.parent else 0.
-        if parent_cost + split_cost < self.cost:
-            delta = np.random.choice(np.arange(len(x)), p=(sum_e / sum_e.sum()))
-            if x[delta] > self.upper[delta]:
-                xi = uniform.rvs(loc=self.upper[delta], scale=x[delta]-self.upper[delta])
-            else:
-                xi = uniform.rvs(loc=x[delta], scale=self.lower[delta]-x[delta])
-
-            j_tilde = MondrianBlock(
-                data=None,
-                parent=self.parent.parent if self.parent else None,
-                budget=self.budget,
-                tree=self.tree,
-                fit=False
-            )
-            j_tilde_attrs = {
-                'delta': delta,
-                'xi': xi,
-                'cost': parent_cost + split_cost,
-                'lower': np.minimum(self.lower, x),
-                'upper': np.maximum(self.upper, x),
-                'sides': np.maximum(self.upper, x) - np.minimum(self.lower, x)
-            }
-
-            for attr, value in j_tilde_attrs.items():
-                setattr(j_tilde, attr, value)
-
-            j_primes = MondrianBlock(np.array([x]), j_tilde, self.budget, self.tree)
-            if x[delta] <= xi:
-                j_tilde.left = j_primes
-                j_tilde.right = self
-            else:
-                j_tilde.left = self
-                j_tilde.right = j_primes
-
-            if self.parent:
-                if self.parent.left is self:
-                    self.parent.left = j_tilde
-                elif self.parent.right is self:
-                    self.parent.right = j_tilde
-
-            self.parent = j_tilde
-            if not j_tilde.parent:
-                self.tree.root = j_tilde
-
-            return j_primes  # ought to be the leaf of x
-        else:
-            self.lower = np.minimum(self.lower, x)
-            self.upper = np.maximum(self.upper, x)
-            if self not in self.tree.leaf_nodes:
-                if x[self.delta] <= self.xi:
-                    child = self.left
-                else:
-                    child = self.right
-                return child.extend(x, y)
-
-    # Algorithm 6
-    def update_posterior_counts(self, y):
-        label_index = self.tree.class_indices[y]
-        self.label_distribution[label_index] += 1
+    # Algorithm 5
+    def _initialize_posterior_counts(self, X, y):
+        for i, cls in enumerate(self.tree.classes):
+            self.class_counts[i] = np.count_nonzero(y == cls)
         current = self
         while True:
-            if self.tables[label_index] == 1:
+            if not current.is_leaf:
+                l_tables = current.left.tables if current.left else np.zeros_like(current.class_counts)
+                r_tables = current.right.tables if current.right else np.zeros_like(current.class_counts)
+                current.class_counts = l_tables + r_tables
+            current.tables = np.minimum(current.class_counts, 1)
+            if current.parent is None:
+                break
+            else:
+                current = current.parent
+
+    # Algorithm 6
+    def _update_posterior_counts(self, y):
+        class_index = self.tree.class_indices[y]
+        self.class_counts[class_index] += 1
+        current = self
+        while True:
+            if current.tables[class_index] == 1:
                 return
             else:
                 if not current.is_leaf:
-                    current.label_distribution[label_index] = 0
-                    current.label_distribution[label_index] += self.left.tables[label_index] if self.left else 0
-                    current.label_distribution[label_index] += self.right.tables[label_index] if self.right else 0
-                    current.tables[label_index] = min(current.label_distribution[label_index], 1)
-                    if current is self.tree.root:
-                        return
+                    l_table = current.left.tables[class_index] if current.left else 0
+                    r_table = current.right.tables[class_index] if current.right else 0
+                    current.class_counts[class_index] = l_table + r_table
+                current.tables[class_index] = np.minimum(current.class_counts[class_index], 1)
+                if current.parent is None:
+                    return
+                else:
+                    current = current.parent
+
+    # Algorithm 9
+    def _fit(self, X, y):
+        self.lower, self.upper = data_ranges(X)
+        self.sides = self.upper - self.lower
+
+        if len(y) <= 0 or np.all(y == y[0]):  # all labels identical
+            self.cost = self.budget
+        else:
+            split_cost = expon.rvs(scale=(1 / self.sides.sum()))
+            self.cost = self._parent_cost() + split_cost
+
+        if self.cost < self.budget:
+            # choose split dimension delta and location xi
+            self.delta = np.random.choice(np.arange(X.shape[1]), p=(self.sides / self.sides.sum()))
+            self.xi = uniform.rvs(loc=self.lower[self.delta], scale=self.sides[self.delta])
+
+            # perform an actual split
+            left_indices = X[:, self.delta] <= self.xi
+            X_left, y_left = X[left_indices], y[left_indices]
+            X_right, y_right = X[~left_indices], y[~left_indices]
+
+            # sample children
+            self.is_leaf = False
+            # we first create unfitted blocks and then fit because otherwise self.left and self.right
+            # may be accessed in ._initialize_posterior_counts before being assigned
+            self.left = MondrianBlock(X_left, y_left, budget=self.budget, parent=self, tree=self.tree, fit=False)
+            self.left._fit(X_left, y_left)
+            self.right = MondrianBlock(X_right, y_right, budget=self.budget, parent=self, tree=self.tree, fit=False)
+            self.right._fit(X_right, y_right)
+        else:
+            self.cost = self.budget
+            self.tree.leaf_nodes.add(self)
+            self._initialize_posterior_counts(X, y)
+
+        self.discount = np.exp(-GAMMA * (self.cost - self._parent_cost()))
+
+    def _get_subset_indices(self):
+        return np.all(self.tree.X >= self.lower, axis=1) & np.all(self.tree.X <= self.upper, axis=1)
+
+    def _get_label_subset(self, indices=None):
+        if indices is None:
+            indices = self._get_subset_indices()
+        return self.tree.y[indices]
+
+    def _get_feature_subset(self, indices=None):
+        if indices is None:
+            indices = self._get_subset_indices()
+        return self.tree.X[indices]
+
+    def _get_feature_label_subset(self, indices=None):
+        if indices is None:
+            indices = self._get_subset_indices()
+        return self._get_feature_subset(indices), self._get_label_subset(indices)
+
+    # Algorithm 10
+    def extend(self, x, y):
+        labels = self._get_label_subset()
+        if len(labels) <= 0 or np.all(labels == labels[0]):  # all labels identical
+            self.lower = np.minimum(self.lower, x)
+            self.upper = np.maximum(self.upper, x)
+            self.tree.X = np.vstack((self.tree.X, x))  # TODO: we possibly don't have to
+            self.tree.y = np.hstack((self.tree.y, y))
+            if y == labels[0]:
+                self._update_posterior_counts(y)
+                return
+            else:
+                self.tree.leaf_nodes.remove(self)
+                X, y = self._get_feature_label_subset()
+                self._fit(X, y)
+        else:
+            el = np.maximum(self.lower - x, np.zeros_like(x))
+            eu = np.maximum(x - self.upper, np.zeros_like(x))
+            sum_e = el + eu
+
+            split_cost = expon.rvs(scale=(1 / sum_e.sum()))
+            if self._parent_cost() + split_cost < self.budget:
+                delta = np.random.choice(np.arange(len(x)), p=(sum_e / sum_e.sum()))
+                if x[delta] > self.upper[delta]:
+                    xi = uniform.rvs(loc=self.upper[delta], scale=x[delta] - self.upper[delta])
+                else:
+                    xi = uniform.rvs(loc=x[delta], scale=self.lower[delta] - x[delta])
+
+                j_tilde = MondrianBlock(None, None, budget=self.budget, parent=self.parent, tree=self.tree, fit=False)
+                j_tilde_attrs = {
+                    'delta': delta,
+                    'xi': xi,
+                    'cost': self._parent_cost() + split_cost,
+                    'lower': np.minimum(self.lower, x),
+                    'upper': np.maximum(self.upper, x),
+                    'sides': np.maximum(self.upper, x) - np.minimum(self.lower, x),
+                    'is_leaf': False,
+                }
+                for attr, value in j_tilde_attrs.items():
+                    setattr(j_tilde, attr, value)
+
+                if self.parent is None:
+                    self.tree.root = j_tilde
+                else:
+                    if self is self.parent.left:
+                        self.parent.left = j_tilde
+                    elif self is self.parent.right:
+                        self.parent.right = j_tilde
+
+                j_primes = MondrianBlock(X=np.array([x]), y=np.array([y]), budget=self.budget,
+                                         parent=j_tilde, tree=self.tree)
+                if x[delta] > xi:
+                    j_tilde.left = self
+                    j_tilde.right = j_primes
+                else:
+                    j_tilde.left = j_primes
+                    j_tilde.right = self
+            else:
+                self.lower = np.minimum(self.lower, x)
+                self.upper = np.maximum(self.upper, x)
+                if not self.is_leaf:
+                    if x[self.delta] <= self.xi:
+                        child = self.left
                     else:
-                        current = current.parent
+                        child = self.right
+                    child.extend(x, y)
 
-
-def plot_mondrian_tree(tree):
-    pass
+    def update_predictive_posterior(self):
+        pass
