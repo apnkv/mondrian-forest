914237af6484a892f8450b2def3fc0d625d322e0
mtaktash <mariataktasheva@gmail.com>
2019-03-21 00:14:02 +0300

    all experiments

--- experiments.py
+++ experiments.py
@@ -1,9 +1,16 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
 from skgarden import MondrianForestClassifier
+from mondrianforest import MondrianRandomForest as OurMondrianForestClassifier
 from data_loaders import load_usps, load_letter, load_satim, load_dna
-from methods import classical_rf_refit, classical_rf_window, classical_rf_incremental, mondrian_rf_skgarden
+from methods import classical_rf_refit, classical_rf_window, classical_rf_incremental
+from methods import mondrian_rf_skgarden, mondrian_rf_our
+
+matplotlib.rcParams.update({'font.size': 15})
+matplotlib.rc('xtick', labelsize=15)
+matplotlib.rc('ytick', labelsize=15)
 
 
 def run_method_on_dataset(method, dataset, n_iter, n_batches, n_estimators, max_depth):
@@ -29,9 +36,9 @@ def run_method_on_dataset(method, dataset, n_iter, n_batches, n_estimators, max_
             clf = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, n_jobs=-1)
             fit_time, train_acc, test_acc = classical_rf_window(clf, dataset, n_batches, h=5)
 
-        elif method == 'classical_rf_increment_frac_0.1':
+        elif method == 'classical_rf_increment_frac_0.2':
             clf = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, warm_start=True)
-            fit_time, train_acc, test_acc = classical_rf_incremental(clf, dataset, n_batches, new_frac=0.1)
+            fit_time, train_acc, test_acc = classical_rf_incremental(clf, dataset, n_batches, new_frac=0.2)
 
         elif method == 'classical_rf_increment_frac_0.5':
             clf = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, warm_start=True)
@@ -53,17 +60,21 @@ def run_method_on_dataset(method, dataset, n_iter, n_batches, n_estimators, max_
             clf = ExtraTreesClassifier(n_estimators=n_estimators, max_depth=max_depth, n_jobs=-1)
             fit_time, train_acc, test_acc = classical_rf_window(clf, dataset, n_batches, h=5)
 
-        elif method == 'extratrees_rf_increment_frac_0.1':
+        elif method == 'extratrees_rf_increment_frac_0.2':
             clf = ExtraTreesClassifier(n_estimators=n_estimators, max_depth=max_depth, warm_start=True)
-            fit_time, train_acc, test_acc = classical_rf_incremental(clf, dataset, n_batches, new_frac=0.1)
+            fit_time, train_acc, test_acc = classical_rf_incremental(clf, dataset, n_batches, new_frac=0.2)
 
         elif method == 'extratrees_rf_increment_frac_0.5':
             clf = ExtraTreesClassifier(n_estimators=n_estimators, max_depth=max_depth, warm_start=True)
             fit_time, train_acc, test_acc = classical_rf_incremental(clf, dataset, n_batches, new_frac=0.5)
 
         elif method == 'mondrian_rf_skgarden':
-            clf = MondrianForestClassifier(n_estimators=n_estimators, max_depth=max_depth, n_jobs=-1)
+            clf = MondrianForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
             fit_time, train_acc, test_acc = mondrian_rf_skgarden(clf, dataset, n_batches)
+
+        elif method == 'mondrian_rf_our':
+            clf = OurMondrianForestClassifier(n_estimators=n_estimators, budget=max_depth)
+            fit_time, train_acc, test_acc = mondrian_rf_our(clf, dataset, n_batches)
 
         mean_fit_time.append(fit_time)
         mean_train_acc.append(train_acc)
@@ -76,52 +87,65 @@ def run_method_on_dataset(method, dataset, n_iter, n_batches, n_estimators, max_
     return mean_fit_time, mean_train_acc, mean_test_acc
 
 
-def run_all_methods_on_dataset(dataset, ax, n_iter, n_batches, n_estimators, max_depth):
+def run_all_methods_on_dataset(dataset, name, n_iter, n_batches, n_estimators, max_depth):
     methods = [
-        'classical_rf_refit',
-        'classical_rf_window_1',
-        'classical_rf_window_3',
-        'classical_rf_window_5',
-        'classical_rf_increment_frac_0.1',
-        'classical_rf_increment_frac_0.5',
-        'extratrees_rf_refit',
-        'extratrees_rf_window_1',
-        'extratrees_rf_window_3',
-        'extratrees_rf_window_5',
-        'extratrees_rf_increment_frac_0.1',
-        'extratrees_rf_increment_frac_0.5',
+        # 'classical_rf_refit',
+        # 'classical_rf_window_1',
+        # 'classical_rf_window_3',
+        # 'classical_rf_window_5',
+        # 'classical_rf_increment_frac_0.2',
+        # 'classical_rf_increment_frac_0.5',
+        # 'extratrees_rf_refit',
+        # 'extratrees_rf_window_1',
+        # 'extratrees_rf_window_3',
+        # 'extratrees_rf_window_5',
+        # 'extratrees_rf_increment_frac_0.2',
+        # 'extratrees_rf_increment_frac_0.5',
         'mondrian_rf_skgarden',
+        'mondrian_rf_our'
     ]
+
+    fig, ax = plt.subplots(1, 3, figsize=(25, 7))
+    plt.suptitle(name)
 
     for method in methods:
 
+        # problem with not enough labels
+        if name == 'satim' and method.startswith('classical_rf_increment'):
+            continue
+
+        elif name == 'satim' and method.startswith('extratrees_rf_increment'):
+            continue
+
+        print(f'\t{method}')
         mean_fit_time, mean_train_acc, mean_test_acc = run_method_on_dataset(method, dataset, n_iter, n_batches,
                                                                              n_estimators, max_depth)
 
-        ax[0].plot(np.arange(n_batches), mean_test_acc, label=method, marker='o')
-        ax[0].set(xlabel='batch num', ylabel='test accuracy')
-        ax[0].legend()
+        ax[0].plot(np.arange(n_batches)/100, mean_test_acc, label=method, marker='o', linewidth=4)
+        ax[0].set(xlabel='fraction of training data', ylabel='test accuracy', title='Test accuracy and fraction of data')
 
-        ax[1].plot(mean_fit_time, mean_test_acc, label=method, marker='o')
-        ax[1].set(xlabel='fit time', ylabel='test accuracy')
+        ax[1].plot(mean_fit_time, mean_test_acc, label=method, marker='o', linewidth=4)
+        ax[1].set(xlabel='training time (seconds)', ylabel='test accuracy', title='Test accuracy and training time')
         ax[1].set_xscale('log')
-        ax[1].legend()
+
+        ax[2].plot([0], [0], label=method)
+        ax[2].set(xticks=[], yticks=[], title='Legend')
+        ax[2].legend(loc='center')
+        ax[2].axis('off')
+
+    plt.show()
 
 
 def run_all_methods_all_datasets(n_iter, n_batches, n_estimators, max_depth):
     datasets = {
-        'usps': load_usps(),
-        'letter': load_letter(),
-        'satim': load_satim(),
-        'dna': load_dna()
+        'usps': load_usps,
+        'letter': load_letter,
+        'satim': load_satim,
+        'dna': load_dna
     }
 
-    fig, ax = plt.subplots(2, 4, figsize=(25, 12))
-
     for i, name in enumerate(datasets):
-        dataset = datasets[name]
+        dataset = datasets[name]()
 
-        ax[0, i].set_title(name)
-        sub_ax = (ax[0, i], ax[1, i])
-
-        run_all_methods_on_dataset(dataset, sub_ax, n_iter, n_batches, n_estimators, max_depth)
+        print(f'Running on {name}...')
+        run_all_methods_on_dataset(dataset, name, n_iter, n_batches, n_estimators, max_depth)
--- methods.py
+++ methods.py
@@ -61,7 +61,7 @@ def classical_rf_window(clf, dataset, n_batches, h=1):
     return fit_time, train_accuracy, test_accuracy
 
 
-def classical_rf_incremental(clf, dataset, n_batches, new_frac=0.1):
+def classical_rf_incremental(clf, dataset, n_batches, new_frac=0.2):
     X_train, X_test, y_train, y_test = dataset
 
     fit_time = []
@@ -91,7 +91,7 @@ def classical_rf_incremental(clf, dataset, n_batches, new_frac=0.1):
         train_accuracy.append(tr_acc)
         test_accuracy.append(test_acc)
 
-        clf.estimators_ = clf.estimators_[n_new:]
+        clf.estimators_ = clf.estimators_[:n_new]
 
     return fit_time, train_accuracy, test_accuracy
 
@@ -116,6 +116,40 @@ def mondrian_rf_skgarden(clf, dataset, n_batches):
 
         t = time.time()
         clf.partial_fit(X_batch, y_batch, classes)
+        ft = time.time() - t
+
+        tr_acc = accuracy_score(y_batch, clf.predict(X_batch))
+        test_acc = accuracy_score(y_test, clf.predict(X_test))
+
+        fit_time.append(ft)
+        train_accuracy.append(tr_acc)
+        test_accuracy.append(test_acc)
+
+    return fit_time, train_accuracy, test_accuracy
+
+
+def mondrian_rf_our(clf, dataset, n_batches):
+    X_train, X_test, y_train, y_test = dataset
+    classes = np.unique(y_train)
+
+    setattr(clf, 'classes', classes)
+
+    fit_time = []
+    train_accuracy = []
+    test_accuracy = []
+
+    n_samples = X_train.shape[0]
+    batch_size = n_samples // n_batches
+
+    for i in range(n_batches):
+        start = max(0, i * batch_size)
+        end = min((i + 1) * batch_size, n_samples)
+
+        X_batch = X_train[start:end]
+        y_batch = y_train[start:end]
+
+        t = time.time()
+        clf.fit(X_batch, y_batch)
         ft = time.time() - t
 
         tr_acc = accuracy_score(y_batch, clf.predict(X_batch))
